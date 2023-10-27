07fa5fede63f833bf68d6cf749f0a5b21f46ee55
Sego <sego@sego.app>
2023-10-28 01:43:43 +0200

    improve stuff
    very helpful git message

--- api/index.js
+++ api/index.js
@@ -1,78 +1,76 @@
 const app = require('express')();
-const PORT = 8080
+const ini = require('ini')
 const cors = require('cors');
 const fs = require('fs');
+const config = ini.parse(fs.readFileSync('../config.ini', 'utf-8'))
+const fsextra = require('fs-extra')
 const axios = require('axios');
-const { createFFmpeg, fetchFile } = require('@ffmpeg/ffmpeg');
-const download = require('download')
+const download = require('download');
+const { FFmpegCommand, FFmpegInput, FFmpegOutput } = require('fessonia')();
 
+const PORT = config.api.port;
+
+app.use(cors());
 app.set('json spaces', 0);
 app.use('/vid', require('express').static('vid'))
-const ffmpeg = createFFmpeg({ log: true });
-async function load() {
-    await ffmpeg.load();
-}
-load();
-app.use(cors());
 app.listen(
-    PORT,
-    () => console.log(`RetroYT API is running on ${PORT}`)
+  PORT,
+  () => console.log(`RetroYT API is running on ${PORT}`)
 )
 
-app.get('/v1/retroyt/flash/:vid', (req, res) => {
-    const { vid } = req.params;
-    axios.get(`https://invidio.xamh.de/api/v1/videos/${vid}?fields=title,description,viewCount,likeCount,author,authorId,subCountText,formatStreams`)
+app.get('/video/:vid', (req, res) => {
+  const { vid } = req.params;
+  axios.get(`https://vid.priv.au/api/v1/videos/${vid}?fields=title,description,viewCount,likeCount,author,authorId,subCountText,formatStreams`)
     .then(data => {
+
       axios.get(`https://returnyoutubedislikeapi.com/votes?videoId=${vid}`)
-      .then(dislikes => {
-        let width = data.data.formatStreams[1].size.split('x')[0];
-        let height = data.data.formatStreams[1].size.split('x')[1];
-  
-        if (fs.existsSync(`./vid/${vid}/videoplayback.flv`)) {
-          res.json({
-            title: data.data.title,
-            desc: data.data.description,
-            views: data.data.viewCount,
-            likes: data.data.likeCount,
-            dislikes: dislikes.data.dislikes, // using Return YouTube Dislikes API in the future
-            channel: data.data.author,
-            channelID: data.data.authorId,
-            subs: data.data.subCountText,
-            width: width,
-            height: height,
-        })
-        } else {
-          download(data.data.formatStreams[1].url, `./vid/${vid}`)
-        .then(() => {
-          (async () => {
-              ffmpeg.FS('writeFile', `videoplayback.mp4`, await fetchFile(`./vid/${vid}/videoplayback.mp4`));
-              await ffmpeg.run('-i', `videoplayback.mp4`, `videoplayback.flv`);
-              await fs.promises.writeFile(`./vid/${vid}/videoplayback.flv`, ffmpeg.FS('readFile', `videoplayback.flv`));
-              // send json
-              res.json({
-                title: data.data.title,
-                desc: data.data.description,
-                views: data.data.viewCount,
-                likes: data.data.likeCount,
-                dislikes: dislikes.data.dislikes, // using Return YouTube Dislikes API in the future
-                channel: data.data.author,
-                channelID: data.data.authorId,
-                subs: data.data.subCountText,
-                width: width,
-                height: height,
+        .then(dislikes => {
+          let width = data.data.formatStreams[1].size.split('x')[0];
+          let height = data.data.formatStreams[1].size.split('x')[1];
+          if (fs.existsSync(`./vid/${vid}/videoplayback.flv`) && fs.existsSync(`./vid/${vid}/videoplayback.wmv`)) {
+            res.json({
+              title: data.data.title,
+              desc: data.data.description,
+              views: data.data.viewCount,
+              likes: data.data.likeCount,
+              dislikes: dislikes.data.dislikes,
+              channel: data.data.author,
+              channelID: data.data.authorId,
+              subs: data.data.subCountText,
+              width: width,
+              height: height,
             })
-            })();
-        });
-        }
-        
-      })
+          } else {
+              download(data.data.formatStreams[1].url, `./vid/${vid}`)
+                .then(() => {
+                  let cmd = new FFmpegCommand();
+                  cmd.addInput(new FFmpegInput(`./vid/${vid}/videoplayback.mp4`));
+                  cmd.addOutput(new FFmpegOutput(`./vid/${vid}/videoplayback.flv`));
+                  cmd.addOutput(new FFmpegOutput(`./vid/${vid}/videoplayback.wmv`));
+                  cmd.spawn();
+                  cmd.on('success', () => {
+                    fsextra.removeSync(`./vid/${vid}/videoplayback.mp4`)
+                    res.json({
+                      title: data.data.title,
+                      desc: data.data.description,
+                      views: data.data.viewCount,
+                      likes: data.data.likeCount,
+                      dislikes: dislikes.data.dislikes,
+                      channel: data.data.author,
+                      channelID: data.data.authorId,
+                      subs: data.data.subCountText,
+                      width: width,
+                      height: height,
+                    })
+                  });
+
+                });
+            }
+          }
+
+        )
     })
     .catch(error => {
       console.log(error.message);
     })
-});
-
-app.get('/v1/retroyt/dl/:vid', (req, res) => {
-    const { vid } = req.params
-    res.sendFile(`${__dirname}/${vid}.swf`)
 });
